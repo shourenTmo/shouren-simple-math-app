a4232c289e95e02f4fec02c8dd38e4cc1d9cdba6
Steven Houren <steven.houren1@t-mobile.com>
2020-11-01 20:52:58 -0500

    Initial commit of simple math app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shouren-simple-math-app';
+
+  public num1: number;
+  public num2: number;
+  public num3: number;
+
+  add() {
+    this.num3=this.num1+this.num2;
+  }
+
+  subtract() {
+    this.num3 = this.num1 - this.num2;
+  }
+
+  multiply() {
+    this.num3 = this.num1 * this.num2;
+  }
+
+  divide() {
+    this.num3 = this.num1 / this.num2;
+  }
+
+
 }
